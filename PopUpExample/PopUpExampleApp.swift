cf0072b22fbb61030306e4a61bd1d891a19f4f72
katafuchix <katafuchix@gmail.com>
2023-01-09 14:20:15 +0900

    Initial Commit

---
+++ PopUpExample/PopUpExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopUpExampleApp.swift
+//  PopUpExample
+//
+//  Created by cano on 2023/01/09.
+//
+
+import SwiftUI
+
+@main
+struct PopUpExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
